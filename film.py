177059a6d8a231bee9007ccf83dd22fb180eefdc
Z-Asroush <102812548+Z-Asroush@users.noreply.github.com>
2022-08-07 19:21:49 +0430

    Delete film.py

--- film.py
+++
@@ -1,6 +0,0 @@
-class Film():
-    def __init__(self, n, di, I, u, du, g ,l):
-        # media.__init__(self,n,di , i , u , du )
-        self.IMDB= I
-        self.leading_actor = l
-        self.genre = g 
