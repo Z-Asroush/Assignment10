db5879afb795fab4234574fc10e62fb67ec51f4b
Z-Asroush <102812548+Z-Asroush@users.noreply.github.com>
2022-08-30 17:38:37 +0430

    Delete clip.py

--- clip.py
+++
@@ -1,8 +0,0 @@
-from Media import media
-class   clip(media):    
-    def __init__(self,media):
-        media.__init__(self,media[0],media[1],media[2],media[3],media[4])    
-        self.format =media[1]
-        self.name=media[2]
-        self.director=media[3]
-        self.duration=media[4]  
