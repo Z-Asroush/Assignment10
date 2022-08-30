7eca16f8e321f338b650dac0f947d188cc70e87c
Z-Asroush <102812548+Z-Asroush@users.noreply.github.com>
2022-08-30 17:38:54 +0430

    Delete documentary.py

--- documentary.py
+++
@@ -1,9 +0,0 @@
-from media import media
-
-class   documentary(media):
-    def __init__(self,media): 
-        media.__init__(self,media[0],media[1],media[2],media[3],media[4] )
-        self.format =media[1]
-        self.name=media[2]
-        self.director=media[3]
-        self.duration=media[4]  
